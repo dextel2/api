1fed938e3ff6ddf67da70902950bb837881919ad
Yash Karanke <16697307+dextel2@users.noreply.github.com>
2022-04-10 21:36:23 +0530

    Added server side validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
 
 /**
  * @access Public
  * @route GET api/users
- * @description Test Route
+ * @description Register User
  */
-router.get("/", (req, res) => {
-	res.send("User route");
+router.post("/", [
+	check('name', "Name is required").not().isEmpty(),
+	check('email', "Please include a valid email").isEmail(),
+	check("password", "Please enter a valid password with 6 or more length").isLength({ min: 6 })
+], (req, res) => {
+
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+
+	res.send('User route');
+
+
 });
 
 module.exports = router;
